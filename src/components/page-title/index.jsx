ec308cf636ec512ccc7710ce83240fac1b3d3fd4
Yang Wang <Yang.Wang04@gmail.com>
2018-03-31 11:12:21 +1100

    Create page-title component

---
+++ src/components/page-title/index.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class PageTitle extends Component {
+  componentWillMount() {
+    document.title = this.props.title + ' | YOU MALL'
+  }
+  render() {
+    return (
+      <div className="row">
+        <div className="col-md-12">
+          <h1 className="page-header">{this.props.title}</h1>
+
+        </div>
+      </div>
+    )
+  }
+}
+
+export default PageTitle
