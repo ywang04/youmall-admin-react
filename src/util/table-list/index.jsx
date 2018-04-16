1e132707857f09f708ef081ef4598f38efd1bad1
Yang Wang <Yang.Wang04@gmail.com>
2018-04-16 16:05:12 +1000

    Create TableList component

---
+++ src/util/table-list/index.jsx
@@ -0,0 +1,60 @@
+import React, { Component } from 'react'
+
+class TableList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      initLoading: true
+    }
+  }
+
+  componentWillReceiveProps() {
+    this.setState({
+      initLoading: false
+    })
+  }
+
+  render() {
+    let tableHeaders = this.props.tableHeaders.map((tableHeader, index) => {
+      return (
+        <th key={index}>
+          {tableHeader}
+        </th >
+      )
+    })
+
+    let listBody = this.props.children
+
+    let listInfo = (
+      <tr>
+        <td colSpan={this.props.tableHeaders.length} className="text-center">
+          {this.state.initLoading ? 'Loading...' : 'No Records...'}
+        </td>
+      </tr>
+    )
+
+    let tableBody = listBody.length > 0 ? listBody : listInfo
+
+    return (
+      <div className="row" >
+        <div className="col-md-10">
+          <table className="table table-striped table-bordered">
+            <thead>
+              <tr>
+                {tableHeaders}
+              </tr>
+            </thead>
+
+            <tbody>
+              {tableBody}
+            </tbody>
+
+          </table>
+        </div>
+      </div>
+
+    )
+  }
+}
+
+export default TableList
