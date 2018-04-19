4b76d6901da4e2fac53fd85228b5c793b6997215
Yang Wang <Yang.Wang04@gmail.com>
2018-04-20 09:15:56 +1000

    Import index.scss into Layout component

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -3,6 +3,7 @@ import NavTop from 'components/nav-top/index.jsx'
 import NavSide from 'components/nav-side/index.jsx'
 
 import './theme.css'
+import './index.scss'
 
 class Layout extends Component {
   constructor(props) {
